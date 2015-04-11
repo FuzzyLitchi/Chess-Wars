64d39428988ec2f64f513a74d3ce2bffdcf64d77
Niels Horn <Niels@Programmer.net>
2015-04-11 21:34:38 +0200

    Add project

---
+++ Project/Assets/Scripts/Environment/ChessBoard.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChessBoard : MonoBehaviour {
+
+	private bool[,] fields;
+
+	void Awake() {
+
+		fields = new bool[64, 64];
+	}
+
+	public void setField(int x, int y) {
+
+		fields 
+	}
+}
